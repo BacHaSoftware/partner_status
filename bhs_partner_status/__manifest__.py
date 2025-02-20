bfff3fa1bc4d5bd62df94f01c2ed18fba1127f8a
Nguyen.Thi.Phuong.Dung <dungntp.bhsoft@gmail.com>
2025-02-20 18:05:09 +0700

    v17: update ava and banner

--- bhs_partner_status/__manifest__.py
+++ bhs_partner_status/__manifest__.py
@@ -14,7 +14,7 @@
     'data': [
         'views/res_partner_views.xml'
     ],
-    'images': ['static/description/banner.gif'],
+    'images': ['static/description/banner.png'],
     # Author
     'author': 'Bac Ha Software',
     'website': 'https://bachasoftware.com',
